b924395b30f467d8bd3ac67096197301eaf7a06e
Oliventer <AOKovalenko2@gmail.com>
2021-02-09 11:21:10 +0200

    Recode highlight using features for Vue

--- frontend/src/views/Snippets.vue
+++ frontend/src/views/Snippets.vue
@@ -3,7 +3,7 @@
       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/tomorrow.min.css">
       <span v-for="note in notes " :key="note.pk">
           <p v-if="note.pk ===  $route.params.pk">
-              <pre v-highlightjs><code>{{note.code}}</code></pre>
+              <highlightjs autodetect :code="note.code" />
           </p>
       </span>
   </div>
