a1a4cd9584fd68cee90a44a91d03761ab7e647ac
Oliventer <AOKovalenko2@gmail.com>
2021-07-01 14:24:53 +0300

    Authorization through passwordless token

--- backend/amirbin/urls.py
+++ backend/amirbin/urls.py
@@ -17,14 +17,16 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from notepad.views import NoteViewSet
 from users.views import UserViewset
-from tokens.views import PasswordlessTokenView
+from tokens.views import PasswordlessTokenView, CheckPaswordlessTokenView, PaswordlessTokenViewSet
 
 router = DefaultRouter()
 router.register('notes', NoteViewSet)
 router.register('users', UserViewset)
+router.register('paswordlessTokens', PaswordlessTokenViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls')),
     path('token/<str:user_email>/', PasswordlessTokenView.as_view()),
+    path('token/check/<str:token_id>/', CheckPaswordlessTokenView.as_view()),
     ]
--- backend/tokens/views.py
+++ backend/tokens/views.py
@@ -1,17 +1,38 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from tokens.serializers import PaswordlessTokenSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from tokens.tasks import send_mail
 from users.models import User
 from tokens.models import PaswordlessToken
+from tokens.serializers import PaswordlessTokenSerializer
+from rest_framework.authtoken.models import Token
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 class PasswordlessTokenView(APIView):
     def get(self, request, user_email, format=None):
         user = User.objects.filter(email = user_email).first()
+        
         if user is None:
-            return Response(data={'No such user registred'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(data = {'No such user registred'}, status=status.HTTP_404_NOT_FOUND)
         
         token = PaswordlessToken.objects.create(user=user)
         send_mail.delay(user_email, "authentification", token.genetate_url())
         return Response(status=status.HTTP_200_OK)
+    
+class CheckPaswordlessTokenView(APIView):
+    def get(self, request, token_id):
+        paswordless_token = PaswordlessToken.objects.get()(token_id=token_id)
+        
+        if paswordless_token is None:
+            return Response(data = {'Token not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        token = Token.objects.create(user=paswordless_token.user)
+        paswordless_token.mark_as_used()
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+        
+class PaswordlessTokenViewSet(ModelViewSet):
+    queryset = PaswordlessToken.objects.all()
+    serializer_class = PaswordlessTokenSerializer
