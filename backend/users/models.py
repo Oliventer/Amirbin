73a55272bec9b9255d34214c921ed4266041dfe0
Oliventer <AOKovalenko2@gmail.com>
2021-07-07 14:31:38 +0300

    Subscription plans on user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from cuser.models import AbstractCUser
 
+class SubscriptionType(models.TextChoices):
+    BASIC = 'B'
+    PREMIUM = 'P'
+    UNLIMITED = 'U'
+
 class User(AbstractCUser):
     password = None
+    subscription = models.TextField(choices=SubscriptionType.choices, default='B')
    
