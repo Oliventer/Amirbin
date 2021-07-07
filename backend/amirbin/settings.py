04cb9d72275aed8259fae241f43eb25779ad6605
Oliventer <AOKovalenko2@gmail.com>
2021-07-07 14:30:32 +0300

    Auto extend session cookie age

--- backend/amirbin/settings.py
+++ backend/amirbin/settings.py
@@ -134,6 +134,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_COOKIE_AGE = 2592000 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
