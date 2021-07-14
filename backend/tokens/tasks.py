d6fa6ea85228da368d33195103eae59935814937
Oliventer <AOKovalenko2@gmail.com>
2021-07-14 13:36:07 +0300

    add mailgun api and domen to .env

--- backend/tokens/tasks.py
+++ backend/tokens/tasks.py
@@ -1,12 +1,17 @@
 from amirbin.celery import app
 import requests
+import environ
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 @app.task
 def send_mail(email, subject, link):
     requests.post(
-        "https://api.mailgun.net/v3/sandbox43bcf9e58ce14bc49aa68b4668eb232a.mailgun.org/messages",
-        auth=("api", "4b86a8ea86619e05ffeaf11a77c0bf71-1f1bd6a9-e10f5225"),
-        data={"from": "Amirbin Api <postmaster@sandbox43bcf9e58ce14bc49aa68b4668eb232a.mailgun.org>",
+        f"https://api.mailgun.net/v3/sandbox{env('MAILGUN_DOMAIN_NAME')}.mailgun.org/messages",
+        auth=("api", env('MAILGUN_API_KEY')),
+        data={"from": f"Amirbin Api <postmaster@sandbox{env('MAILGUN_DOMAIN_NAME')}.mailgun.org>",
               "to": f"{email}",
               "subject": f"Amirbin {subject}",
               "html": f"Click on the link for {subject}: {link}" })
