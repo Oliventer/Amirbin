b82af814b4d1050952bc2267c8998a38c45fca75
Oliventer <AOKovalenko2@gmail.com>
2021-07-16 14:21:47 +0300

    Transfer stripe checkout session creation to service object

---
+++ backend/subscriptions/services/stripe_checkout.py
@@ -0,0 +1,30 @@
+from amirbin import settings
+import stripe
+
+products = {'Premium': 'price_1JDCyVB9cEHLnTYOxmajeYKc', 'Unlimited': 'price_1JDD1JB9cEHLnTYOn9k5yFz1'}
+
+
+class StripeCheckoutService:
+    def __init__(self, user_id, product_name):
+        self.user_id = user_id
+        self.product_name = product_name
+        
+    def __call__(self):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        price_id = products[self.product_name]
+        
+        session = checkout_session = stripe.checkout.Session.create(
+                client_reference_id = self.user_id,
+                success_url = settings.FRONTEND_URL + 'notes/1/',
+                cancel_url = settings.FRONTEND_URL + 'notes/2/',
+                payment_method_types =['card'],
+                mode ='subscription',
+                line_items = [
+                    {
+                        'price': price_id,
+                        'quantity': 1,
+                    }
+                ]
+            )
+        return session
+        
--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,36 +1,23 @@
 from rest_framework.views import APIView
-import stripe
 from amirbin import settings
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import redirect
 from users.models import User
+from subscriptions.services.stripe_checkout import StripeCheckoutService
+from rest_framework.permissions import IsAuthenticated
 
-products = {'Premium': 'price_1JDCyVB9cEHLnTYOxmajeYKc', 'Unlimited': 'price_1JDD1JB9cEHLnTYOn9k5yFz1'}
 
 class StripeKeyView(APIView):
     def get(seld, request):
         return Response({'stripe_key': settings.STRIPE_PUBLISHABLE_KEY}, status=status.HTTP_200_OK)
 
 class CheckoutSessionCreationView(APIView):
+    permission_classes = [IsAuthenticated]
+    
     def post(self, request, product_name):
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        price_id = products[product_name]
-        
         try:
-            checkout_session = stripe.checkout.Session.create(
-                client_reference_id= # request.user.id if request.user.is_authenticated else None,
-                success_url= settings.FRONTEND_URL + 'notes/1/',
-                cancel_url= settings.FRONTEND_URL + 'notes/2/',
-                payment_method_types=['card'],
-                mode='subscription',
-                line_items=[
-                    {
-                        'price': price_id,
-                        'quantity': 1,
-                    }
-                ]
-            )
+            checkout_session = StripeCheckoutService(request.user.id, product_name)()
             return Response({'sessionId': checkout_session['id']}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
